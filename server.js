2acaf71b16641a9f1b91f0dd4fdb76483aed3907
Janet <Reid>
2020-01-05 11:11:36 +0800

    Initialised NPM method-override

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var methodOverride = require("method-override");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());(
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//Import routes and give the server access to them
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
