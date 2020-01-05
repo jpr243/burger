f0df819db8aaf921fdbf4bef774261b8e437ad59
Janet <Reid>
2020-01-05 13:21:19 +0800

    Fixed if statement coding eror

--- config/orm.js
+++ config/orm.js
@@ -15,10 +15,10 @@ function translateSQL(ob) {
   for (var key in ob) {
     var value = ob[key];
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if (typeof value === "string" && value.indexOf(" ") >= 0) {
-      //   value = "'" + value + "'";
-      // }
-      arr.push(key + "=" + ob[key]);
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
     }
   }
   return arr.toString();
