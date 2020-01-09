9c92e2009d4584483fa37ebe5fdd5da50951ce29
Janet <Reid>
2020-01-09 20:02:36 +0800

    HEROKU PLS WORK

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 var mysql = require("mysql");
 
 //Connection
-connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "OliverHank6",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "OliverHank6",
+    database: "burgers_db"
+  });
+}
 //start connection to database
 connection.connect(function(err) {
   if (err) throw err;
