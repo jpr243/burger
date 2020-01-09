4eca180301970d7b7ffd7f56081f011110cdb2a6
Janet <Reid>
2020-01-09 20:05:04 +0800

    second times the charm

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,8 @@
 var mysql = require("mysql");
 
 //Connection
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "OliverHank6",
-    database: "burgers_db"
-  });
-}
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 //start connection to database
 connection.connect(function(err) {
   if (err) throw err;
